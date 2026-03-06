040828667aa034a1345cc029300054f40884efa5
Heriberto Hernandez <heribertobta.z@email.com>
2026-03-06 03:32:19 -0600

    refactor: elimina enlace a reportes en el componente de navegación secundaria

--- public/componenetes_js/componentes.js
+++ public/componenetes_js/componentes.js
@@ -65,9 +65,6 @@ class NavSecundario extends HTMLElement {
                 <a class="pill ${activeClass('usuario')}" href="${toFolder}usuarios.html">
                     REGISTROS <span class="ico">👤</span>
                 </a>
-                 <a class="pill ${activeClass('reportes')}" href="${toFolder}reportes.html">
-                    REPORTES <span class="ico">👤</span>
-                </a>
             </div>
           </div>
         </nav>`;
